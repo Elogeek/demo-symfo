f21e054aab9daab0686a34203ee19d3778b6f872
elogeek <elodiechristin@gmail.com>
2022-02-09 09:04:01 +0100

    debbug symfony suite

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -15,15 +15,7 @@ class ArticleController extends AbstractController {
      * Return all articles
      */
     public function list(): Response {
-
-        $articles = [
-            new ArticleController(),
-            new ArticleController(),
-            new ArticleController(),
-        ];
-
-        return $this->render('article/list.html.twig', ['articles' => $articles]);
-
+        return $this->render('article/list.html.twig');
     }
 
     /**
@@ -31,22 +23,14 @@ class ArticleController extends AbstractController {
      */
     #[Route('/show/{articleId<\d+>}', name: 'show', methods: ['GET'])]
     public function show(int $articleId): Response {
-        $articles = [
-            new ArticleController()
-        ];
-
-        return $this->render('article/single.html.twig', ['articles' => $articles]);
-
+        return $this->render('article/single.html.twig', ['articleId' => $articleId]);
     }
 
     /**
      * Edit a single article
      */
-     #[Route('/edit/{articleID<\d+>}', name: 'edit', methods: ['GET', 'POST'])]
+    #[Route('/edit/{articleID<\d+>}', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(int $articleID): Response {
-       $articles = [
-           new ArticleController()
-       ];
 
       /**
        * If not admin === redirectTO home article (articles_list)
@@ -55,19 +39,15 @@ class ArticleController extends AbstractController {
            return $this->redirectToRoute('articles_list');
        }
        */
-       return $this->render('article/edit.html.twig', ['articles' => $articles]);
+       return $this->render('article/edit.html.twig', ['articleID' => $articleID]);
     }
 
-     /**
-      * Delete a single article
-      */
-      #[Route('/delete/{id<\d+>}', name: 'delete', methods: ['GET', 'DELETE'])]
+    /**
+     * Delete a single article
+     */
+    #[Route('/delete/{id<\d+>}', name: 'delete', methods: ['GET', 'DELETE'])]
     public function delete(int $id): Response {
-        $articles = [
-          new ArticleController()
-        ];
-
-        return $this->render('article/delete.html.twig', ['articles' => $articles]);
+        return $this->render('article/delete.html.twig', ['id' => $id]);
     }
 
 }
